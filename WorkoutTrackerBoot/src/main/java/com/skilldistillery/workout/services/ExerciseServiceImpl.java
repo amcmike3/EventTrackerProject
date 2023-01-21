6c89074d5fe4e6ea9f17d568bc166ec1f1edb82f
Amcmike3 <amcmichael3@gmail.com>
2023-01-20 22:45:14 -0700

    implement stubbed out methods int exercise service impl

--- WorkoutTrackerBoot/src/main/java/com/skilldistillery/workout/services/ExerciseServiceImpl.java
+++ WorkoutTrackerBoot/src/main/java/com/skilldistillery/workout/services/ExerciseServiceImpl.java
@@ -1,42 +1,68 @@
 package com.skilldistillery.workout.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.workout.entities.Exercise;
+import com.skilldistillery.workout.repositories.ExerciseRepository;
 
 @Service
 public class ExerciseServiceImpl implements ExerciseService {
+	
+	@Autowired
+	private ExerciseRepository exRepo;
 
 	@Override
 	public List<Exercise> allExercises() {
-		// TODO Auto-generated method stub
-		return null;
+		return exRepo.findAll();
 	}
 
 	@Override
 	public Exercise getExercise(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Exercise> opt = exRepo.findById(id);
+		Exercise ex = null;
+		if (opt.isPresent()) {
+			ex = opt.get();		
+			}
+		return ex;
 	}
 
 	@Override
 	public Exercise createExercise(Exercise exercise) {
-		// TODO Auto-generated method stub
-		return null;
+		Exercise ans = null;
+		if (exercise != null) {
+			ans = exRepo.saveAndFlush(exercise);
+		}
+		return ans;
 	}
 
 	@Override
 	public Exercise updateExercise(int id, Exercise exercise) {
-		// TODO Auto-generated method stub
-		return null;
+		Exercise ans = null;
+		Optional<Exercise> opt = exRepo.findById(id);
+		if(opt.isPresent()) {
+			ans = opt.get();
+			if(exercise.getDescription() != null && !exercise.getDescription().equals("")) {
+				ans.setDescription(exercise.getDescription());
+			}
+			if(exercise.getWeight() != null) {
+				ans.setWeight(exercise.getWeight());
+			}
+			if(exercise.getName() != null && !exercise.getName().equals("")) {
+				ans.setName(exercise.getName());
+			}
+			exRepo.saveAndFlush(ans);
+		}
+		return ans;
 	}
 
 	@Override
 	public boolean deleteExerciseById(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		exRepo.deleteById(id);
+		return exRepo.findById(id).isPresent();
 	}
 
 }

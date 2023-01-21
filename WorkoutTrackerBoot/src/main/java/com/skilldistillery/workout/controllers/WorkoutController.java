3a5d6f686a103f0e64d38830fb066e8ded30a06e
Amcmike3 <amcmichael3@gmail.com>
2023-01-20 23:01:30 -0700

    add REST api mapping methods to workout and exercise controllers

--- WorkoutTrackerBoot/src/main/java/com/skilldistillery/workout/controllers/WorkoutController.java
+++ WorkoutTrackerBoot/src/main/java/com/skilldistillery/workout/controllers/WorkoutController.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.workout.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.workout.entities.Workout;
 import com.skilldistillery.workout.services.WorkoutService;
 
 @RestController
@@ -12,5 +16,10 @@ public class WorkoutController {
 	
 	@Autowired
 	private WorkoutService workServ;
+	
+	@GetMapping("workouts")
+	public List<Workout> allWorkouts(){
+		return workServ.allWorkouts();
+	}
 
 }

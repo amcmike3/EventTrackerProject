fa325f16f8db00ae19077ae1b654ef3355c5576b
Amcmike3 <amcmichael3@gmail.com>
2023-02-04 18:50:12 -0700

    add date pipes to dates

--- ngWorkoutTracker/src/app/models/workout.ts
+++ ngWorkoutTracker/src/app/models/workout.ts
@@ -1,12 +1,12 @@
 import { Exercise } from './exercise';
 export class Workout {
   id : number;
-  date : string;
+  date : Date;
   mood : string;
   notes :string;
   enabled : boolean;
-  exercises : Exercise[];
-  constructor(id : number = 0, date : string = "", mood : string= "", notes :string= "",enabled : boolean = true ,exercises : Exercise[] = []){
+  exercises : Exercise[] | null;
+  constructor(id : number = 0, date : Date = new Date(), mood : string= "", notes :string= "",enabled : boolean = true ,exercises : Exercise[] | null = null){
     this.id = id;
     this.date = date;
     this.mood = mood;
